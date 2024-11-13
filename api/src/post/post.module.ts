abd33375a5153cecfe3ae38b5dba66945bf38d17
MesQue1041 <abdulrahuman1041@gmail.com>
2024-11-13 21:04:28 +0530

    TypeOrm setup

--- api/src/post/post.module.ts
+++ api/src/post/post.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    PostModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      database: "JBlog",
+      username: "root",
+      password: "Abdul@1041",
+      autoLoadEntities: true,
+      port: 3306,
+      synchronize: true
+    })
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
