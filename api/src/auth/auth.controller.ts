3074dcc553563dfbedea31d1469b009b5883a7c2
MesQue1041 <abdulrahuman1041@gmail.com>
2024-11-19 13:22:42 +0530

    Authentication fully complete

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,10 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async userLogin(@Body() userLoginDto: UserLoginDto, @Res() res: Response) {
+    const {token, user} = await this.authService.login(userLoginDto);
+
+    res.cookie('IsAuthenicated', true,  {maxAge: 2*690*60*1000})  //max age 2 hours
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      maxAge: 2*690*60*1000
+    });
+
+    return res.send({ sucess: true, user});
+  }
 
 }
