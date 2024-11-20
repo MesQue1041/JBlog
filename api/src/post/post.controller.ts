64eaa62ccc71b8daa052a2fe166f9265ffdbae63
MesQue1041 <abdulrahuman1041@gmail.com>
2024-11-20 13:33:51 +0530

    Fixed some bugs

--- api/src/post/post.controller.ts
+++ api/src/post/post.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, UseInterceptors, ClassSerializerInterceptor, ValidationPipe, Req, Query, UseGuards, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, UseInterceptors, ClassSerializerInterceptor, ValidationPipe, Req, Query, UseGuards, UploadedFile, BadRequestException, Res } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import { User } from 'src/auth/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/auth/user.decorator';
@@ -43,29 +43,40 @@ export class PostController {
   }
 
   @Post('upload-photo')
-  @UseInterceptors(FileInterceptor('file', {
-    storage: diskStorage({
-      destination: './uploads',
-      filename: (req, file, cb) => {
-        const name = file.originalname.split('')[0];
-        const fileExtension = file.originalname.split('')[1];
-        const newFileName = name.split(" ").join("_") + "_" + Date.now() + "." + fileExtension;
+@UseInterceptors(FileInterceptor('file', {
+  storage: diskStorage({
+    destination: './uploads',
+    filename: (req, file, cb) => {
+      const name = file.originalname.split('.')[0]; // Get the name before the first dot
+      const fileExtension = file.originalname.split('.').pop(); // Get the extension after the last dot
+      const newFileName = name.split(" ").join("_") + "_" + Date.now() + "." + fileExtension; // Construct the filename
 
-        cb(null, newFileName);
-      }
-    }),
-    fileFilter: (req, file, cb) => {
-      if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return cb(null, false);
-      }
-      cb(null, true)
-      
+      cb(null, newFileName); // Pass the new filename to the callback
     }
-
-  }))
-  uploadPhoto(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  }),
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) { // Allow only specific image formats
+      return cb(new BadRequestException('Only image files are allowed!'), false);
+    }
+    cb(null, true); // Accept the file
   }
+}))
+uploadPhoto(@UploadedFile() file: Express.Multer.File) {
+  if (!file) {
+    throw new BadRequestException('File is not an image');
+  } else {
+    const response = {
+      filepath: `http://localhost:5000/post/pictures/${file.filename}` // Construct the response
+    };
+    return response;
+  }
+}
+
+  @Get('pictures/:filename')
+  async getPicture(@Param('filename') filename, @Res() res: Response){
+    res.sendFile(filename, {root: './uploads'});
+  }
+
 
   @Patch(":slug")
   @UseGuards(AuthGuard('jwt'))
