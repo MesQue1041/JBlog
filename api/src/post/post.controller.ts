8f799864a0aa2dd8afdbd453113fc27da78f1fbf
MesQue1041 <abdulrahuman1041@gmail.com>
2024-11-20 13:23:16 +0530

    File upload working

--- api/src/post/post.controller.ts
+++ api/src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, UseInterceptors, ClassSerializerInterceptor, ValidationPipe, Req, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, UseInterceptors, ClassSerializerInterceptor, ValidationPipe, Req, Query, UseGuards, UploadedFile } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
@@ -7,6 +7,9 @@ import { User } from 'src/auth/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/auth/user.decorator';
 import { CurrentUserGuard } from 'src/auth/current-user.guard';
+import { Express } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('post')
 export class PostController {
@@ -37,6 +40,31 @@ export class PostController {
   @Get("/slug/:slug")
   findBySlug(@Param('slug') slug: string) {
     return this.postService.findBySlug(slug);
+  }
+
+  @Post('upload-photo')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req, file, cb) => {
+        const name = file.originalname.split('')[0];
+        const fileExtension = file.originalname.split('')[1];
+        const newFileName = name.split(" ").join("_") + "_" + Date.now() + "." + fileExtension;
+
+        cb(null, newFileName);
+      }
+    }),
+    fileFilter: (req, file, cb) => {
+      if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+        return cb(null, false);
+      }
+      cb(null, true)
+      
+    }
+
+  }))
+  uploadPhoto(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 
   @Patch(":slug")
