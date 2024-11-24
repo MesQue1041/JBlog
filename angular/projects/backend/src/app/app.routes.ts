80d94714237e5e0b9ca0addfea32759a81b76fcb
MesQue1041 <abdulrahuman1041@gmail.com>
2024-11-24 22:09:01 +0530

    Admin backend components initialised

--- angular/projects/backend/src/app/app.routes.ts
+++ angular/projects/backend/src/app/app.routes.ts
@@ -1,3 +1,64 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NewPostComponent } from './components/posts/new-post/new-post.component';
+import { EditPostComponent } from './components/posts/edit-post/edit-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'posts',
+                pathMatch: 'full'
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'posts',
+                children: [
+                    {
+                        path: '',
+                        component: PostsComponent
+                    },
+                    {
+                        path: 'categories',
+                        component: CategoryComponent
+                    },
+                    {
+                        path: 'create',
+                        component: NewPostComponent
+                    },
+                    {
+                        path: 'edit/:slug',
+                        component: EditPostComponent
+                    },
+                    {
+                        path: '**',
+                        redirectTo: '',
+                        pathMatch: 'full'
+                    },
+                    
+                ]
+            },
+
+        ]
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+
+];
