7b0fd956c9af0ff3a5126d9d0bd79abda67b98c9
MesQue1041 <abdulrahuman1041@gmail.com>
2024-11-24 22:46:52 +0530

    SCSS issues fixed

--- angular/projects/backend/src/app/components/main/main.component.ts
+++ angular/projects/backend/src/app/components/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../../../frontend/src/app/components/header/header.component";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
